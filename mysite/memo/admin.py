788d5a43d607df12462025aaa7b9f665e137aff6
comssa <comssa56@gmail.com>
2022-05-19 00:42:20 +0900

    mod: add editing tips function in memo admin

--- mysite/memo/admin.py
+++ mysite/memo/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 
-from .models import Memo
+from .models import Memo, Tips
 
-admin.site.register(Memo)
+class TipsInline(admin.StackedInline):
+    model = Tips
+    extra = 3
+
+class MemoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['memo_text']}),
+        ('Date information', {'fields': ['insert_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [TipsInline]
+
+admin.site.register(Memo, MemoAdmin)
